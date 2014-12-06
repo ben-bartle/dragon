e3b700f35065d6690671505f1cc68be2fcaece3e
ben-bartle <benjamin.d.bartle@gmail.com>
2014-12-06 11:32:11 -0600

    yeoman angular scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dragonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dragonApp
+ */
+angular.module('dragonApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
